10517b9d20112f71dafa172ea54376acd29a0f2a
yeeun4028 <ywj4028@163.com>
2026-03-11 22:39:28 +0800

    解耦FileListView (Block2)

---
+++ Features/Main/Components/FileListView.xaml.cs
@@ -0,0 +1,65 @@
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using PromptMasterv6.Features.Shared.Models;
+
+namespace PromptMasterv6.Features.Main.Components
+{
+    public partial class FileListView : System.Windows.Controls.UserControl
+    {
+        public FileListView()
+        {
+            InitializeComponent();
+        }
+
+        private void FileInlineEditor_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (sender is System.Windows.Controls.TextBox textBox && textBox.DataContext is PromptItem promptItem && promptItem.IsRenaming)
+            {
+                textBox.Focus();
+                textBox.SelectAll();
+            }
+        }
+
+        private void FileInlineEditor_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (sender is System.Windows.Controls.TextBox textBox && textBox.DataContext is PromptItem promptItem)
+            {
+                if (e.Key == System.Windows.Input.Key.Enter)
+                {
+                    promptItem.IsRenaming = false;
+                    if (DataContext is MainViewModel vm)
+                    {
+                        vm.FileManagerVM.RequestSaveCommand.Execute(null);
+                    }
+                    e.Handled = true;
+                }
+                else if (e.Key == System.Windows.Input.Key.Escape)
+                {
+                    if (string.IsNullOrWhiteSpace(promptItem.Title))
+                    {
+                        promptItem.Title = "未命名提示词";
+                    }
+                    promptItem.IsRenaming = false;
+                    e.Handled = true;
+                }
+            }
+        }
+
+        private void FileInlineEditor_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (sender is System.Windows.Controls.TextBox textBox && textBox.DataContext is PromptItem promptItem)
+            {
+                if (string.IsNullOrWhiteSpace(promptItem.Title))
+                {
+                    promptItem.Title = "未命名提示词";
+                }
+                promptItem.IsRenaming = false;
+                if (DataContext is MainViewModel vm)
+                {
+                    vm.FileManagerVM.RequestSaveCommand.Execute(null);
+                }
+            }
+        }
+    }
+}
