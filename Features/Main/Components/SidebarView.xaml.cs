a74d366ccbc99ded8292947a3bd236dbf204555c
yeeun4028 <ywj4028@163.com>
2026-03-11 22:46:56 +0800

    解耦 Block1 后

---
+++ Features/Main/Components/SidebarView.xaml.cs
@@ -0,0 +1,10 @@
+namespace PromptMasterv6.Features.Main.Components
+{
+    public partial class SidebarView : System.Windows.Controls.UserControl
+    {
+        public SidebarView()
+        {
+            InitializeComponent();
+        }
+    }
+}
